d3143608ed32a4d99aa72c0e57f4f8fc45d6ff1e
Colin Schlecht <schlechtcolin@yahoo.com>
2021-03-28 19:28:10 -0700

    adds brewery controller functionality

--- app/controllers/api/v1/breweries_controller.rb
+++ app/controllers/api/v1/breweries_controller.rb
@@ -1,2 +1,19 @@
 class Api::V1::BreweriesController < ApplicationController
+
+    #all breweries endpoint has a long load time
+    def index
+        breweries = Brewery.all 
+        render json: breweries
+    end
+
+    #used for all breweries by state
+    def state  
+        breweries = Brewery.all.where(state: params[:state])
+        render json: breweries
+    end
+
+    private
+    # will need to add strong params if going further with CRUD for breweries
 end
+
+
--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,10 +1,3 @@
 class Api::V1::UsersController < ApplicationController
-    def index
-        breweries = Brewery.all 
-        render json: breweries
-    end
-
-    def state  
-        breweries = Brewery.all.
-    end
+   
 end
