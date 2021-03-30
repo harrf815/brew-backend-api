114c0b4a92c19395af86caa2ce7faf79f8bae735
Colin Schlecht <schlechtcolin@yahoo.com>
2021-03-30 16:40:11 -0700

    working sign up and login

--- app/controllers/api/v1/auth_controller.rb
+++ app/controllers/api/v1/auth_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::AuthController < ApplicationController
     skip_before_action :authorized, only: [:login]
   def login
     user = User.find_by(username: params[:username])
-    if user && user.authenticate(params[:password])
+    if user && user.authenticate(params[:password])                                       
       token = issue_token({user_id: user.id})
       render json: {user: UserSerializer.new(user), jwt: token}, status: :accepted
     else
--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -12,7 +12,7 @@ class Api::V1::UsersController < ApplicationController
       @user = User.create(user_params)
       if @user.valid?
         @token = issue_token({ user_id: @user.id })
-        render json: { user: UserSerializer.new(@user), token: @token }, status: :created
+        render json: { user: UserSerializer.new(@user), jwt: @token }, status: :created
       else
         render json: { error: 'failed to create user' }, status: :not_acceptable
       end
@@ -25,4 +25,3 @@ class Api::V1::UsersController < ApplicationController
       params.permit(:username, :password, :zip)
     end
 end 
-
