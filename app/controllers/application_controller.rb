20c8c0991f605e94867e45671e848556ae478265
Colin Schlecht <schlechtcolin@yahoo.com>
2021-03-29 21:50:49 -0700

    changes secret

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::API
     before_action :authorized
 
     def issue_token(payload)
-        JWT.encode(payload, ENV['JWT_SECRET'])
+        JWT.encode(payload, 'secret')
     end
 
     def auth_header
@@ -13,7 +13,7 @@ class ApplicationController < ActionController::API
         if auth_header
             token = auth_header.split(' ')[1]
             begin
-                JWT.decode(token, ENV['JWT_SECRET'])
+                JWT.decode(token, 'secret', true, algorithm: 'HS256')
             rescue JWT::DecodeError
                 nil
             end
